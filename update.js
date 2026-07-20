bd687b87981bf56167fe91b5a77da2ddb2377134
GILDO ADRIANO NORBERTO DA SILVA <133284362+GildoAdrianoZERO@users.noreply.github.com>
2026-07-20 19:56:47 -0300

    JS

---
+++ update.js
@@ -0,0 +1,9 @@
+
+
+function iniciarDesafio(nome) {
+  const dataAtual = new Date().toLocaleDateString('pt-BR');
+  console.log(`🚀 Olá, ${nome}! Desafio iniciado com sucesso em ${dataAtual}.`);
+  console.log("Código pronto para o primeiro commit no GitHub!");
+}
+
+iniciarDesafio("Gildo");
